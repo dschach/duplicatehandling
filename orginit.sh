92f8b4cce25db9fca866c2ce84374df185e09d1a
dschach <dschach@x2od.com>
2021-07-08 14:37:17 -0700

    Setup file updates

---
+++ orginit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sfdx force:org:create -f config/project-scratch-def.json --durationdays 7 -a ActionPlans -s
+
+sfdx force:source:push
+
+sfdx force:user:permset:assign -n Duplicates_Handler
+
+sfdx force:org:open
+
+echo "Org is set up"
+
+#Version Increase Comment
+
+
+#Deploy to ActionPlans Dev/Packaging Org: Use -c to check only first
+#
+#sfdx force:source:deploy -u ActionPlansPackagingOrg -w 100 -p force-app/main/default
