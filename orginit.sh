1597aa37c1e284e4961e20467831c3fe773e2a96
David Schach <dschach@x2od.com>
2021-10-05 19:47:48 -0700

    Clean orginit

--- orginit.sh
+++ orginit.sh
@@ -9,9 +9,3 @@ sfdx force:user:permset:assign -n Duplicates_Handler
 sfdx force:org:open
 
 echo "Org is set up"
-
-#Version Increase Comment
-
-#Deploy to ActionPlans Dev/Packaging Org: Use -c to check only first
-#
-#sfdx force:source:deploy -u ActionPlansPackagingOrg -w 100 -p force-app/main/default
